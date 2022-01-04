be787e53a4cda2414921d5df420b682f24b903ad
Vaithidroid <vaithisowmi959@gmail.com>
2022-01-04 13:20:18 +0530

    Initial commit

---
+++ app/src/main/java/com/vaithidroid/appone/how2cook/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vaithidroid.appone.how2cook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
